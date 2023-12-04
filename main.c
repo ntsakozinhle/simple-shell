1f088c43bfd74417fe2b25ecc2dc8721079e3ac4
ntsakozinhle <ntsakosambo1@gmail.com>
2023-12-04 04:39:37 -0800

    Simple shell 0.1

--- main.c
+++ main.c
@@ -10,16 +10,61 @@
 int main(int ac, char **argv)
 {
 	char *prompt = "shell ~$ ";
-	char *lineptr;
+	char *lineptr = NULL, *lineptr_cpy = NULL;
 	size_t n = 0;
+	ssize_t nchars;
+	const char *delim = " \n";
+	int num_tokens = 0;
+	char *token;
+	int i;
 
 	(void)ac;
-	(void)argv;
 
-	printf("%s", prompt);
-	getline(&lineptr, &n, stdin);
-	printf("%s\n", lineptr);
+	while (1)
+	{
+		printf("%s", prompt);
+		nchars = getline(&lineptr, &n, stdin);
+		
+		if (nchars == -1)
+		{
+			printf("Exiting shell....\n");
+			return (-1);
+		}
 
-	free(lineptr);
+		lineptr_cpy = malloc(sizeof(char) * nchars);
+		if (lineptr_cpy == NULL)
+		{
+			perror("tsh: memory allocation error");
+			return (-1);
+		}
+
+		strcpy(lineptr_cpy, lineptr);
+
+		token = strtok(lineptr, delim);
+
+		while (token != NULL)
+		{
+			num_tokens++;
+			token = strtok(NULL, delim);
+		}
+		num_tokens++;
+
+		argv = malloc(sizeof(char *) * num_tokens);
+
+		token = strtok(lineptr_cpy, delim);
+
+		for (i = 0; token != NULL; i++)
+		{
+			argv[i] = malloc(sizeof(char) * strlen(token));
+			strcpy(argv[i], token);
+
+			token = strtok(NULL, delim);
+		}
+		argv[i] = NULL;
+
+		printf("%s\n", lineptr);
+
+		free(lineptr);
+	}
 	return (0);
 }
